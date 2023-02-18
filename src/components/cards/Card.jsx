6de9180bc9dc7ccd9da7290d7812d3902dbd9711
Yusuf <baliyusuf675@gmail.com>
2023-02-18 18:01:38 +0300

    end

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Card.css";
+import { data } from "../../helper/data";
+
+const Card = () => {
+  console.log(data);
+  return (
+    <div className="cont">
+      <div className="container">
+        {data.map((item) => {
+          const { id, title, desc, image } = item;
+          return (
+            <div className="card">
+              <h1 className="title">{title}</h1>
+              <img src={image} height="350px" alt="" />
+              <p>{desc}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <a>About US</a>
+      <a>For You</a>
+      <a>SERVİCES</a>
+      <a>BLOG</a>
+      <a>VLOG</a>
+      <a>CONTACT</a>
+    </div>
+  );
+};
+
+export default Navbar;
